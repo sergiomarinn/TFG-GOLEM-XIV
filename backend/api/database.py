91c0b85412830283852ac15713547861dc6baee4
GerardLopezRuiz <gerardlopez201@gmail.com>
2024-01-16 01:53:03 +0100

    amagat contrasenya de base de dades

--- backend/api/database.py
+++ backend/api/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.sql import text
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Gerard021101_@localhost:5432/correcion"
+SQLALCHEMY_DATABASE_URL = os.getenv("url_db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL

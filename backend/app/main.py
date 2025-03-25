9a4f67944e6a5eea16253632f60d66ca8986dcc6
sergiomarinn <sergiomarin793@gmail.com>
2025-03-25 12:33:58 +0100

    Refactor main application entry point

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,37 +1,33 @@
-"""Es el punto de entrada principal de la aplicación FastAPI en tu proyecto.
-El propósito del archivo es levantar el servidor FastAPI y manejar las rutas mediante 
-la inclusión de los routers definidos en otras partes del proyecto."""
+import sentry_sdk
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from starlette.middleware.cors import CORSMiddleware
+
+from app.api.main import api_router
+from app.core.config import settings
 
 
-import fastapi
-from api.auth.controller import router as correctorRouter
-from api.cursos.controller import router as cursosRouter
-from fastapi.middleware.cors import CORSMiddleware
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
 
 
-app = fastapi.FastAPI()
+if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
+    sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
 
-app.include_router(correctorRouter)
-app.include_router(cursosRouter)
-
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["Content-Disposition"]
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
 )
 
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-@app.get("/")
-def index():
-    return {"name": app.title, "version": app.version}
-
-
+app.include_router(api_router, prefix=settings.API_V1_STR)

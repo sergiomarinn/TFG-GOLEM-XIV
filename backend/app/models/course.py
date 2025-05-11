d4841ef87b139b0f18278dfcbb0264fe06f597b4
sergi <sergiomarin793@gmail.com>
2025-05-11 20:46:25 +0200

    Add students_number property to Course model for student count calculation

--- backend/app/models/course.py
+++ backend/app/models/course.py
@@ -1,5 +1,7 @@
 from sqlmodel import Field, Relationship, Enum, Column
 from pydantic import model_validator
+from sqlalchemy.ext.hybrid import hybrid_property
+from typing import ClassVar
 
 from .base import SQLModel
 from .CoursesUsersLink import CoursesUsersLink
@@ -35,6 +37,11 @@ class Course(CourseBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     users: list[User] = Relationship(back_populates="courses", link_model=CoursesUsersLink)
     practices: list["Practice"] = Relationship(back_populates="course", cascade_delete=True)
+    students_number: ClassVar[hybrid_property]
+    
+    @hybrid_property
+    def students_number(self) -> int:
+        return sum(user.is_student for user in self.users if hasattr(user, "is_student"))
 
 class CourseCreate(CourseBase):
     # This validator ensures that if the input is a JSON string, it gets parsed and converted to the appropriate model instance (mostly in form-data request)
@@ -57,6 +64,7 @@ class CoursePublic(CourseBase):
     id: uuid.UUID
     corrected_practices: int = 0
     total_practices: int = 0
+    students_number: int
 
 class CoursePublicWithUsersAndPractices(CourseBase):
     id: uuid.UUID
